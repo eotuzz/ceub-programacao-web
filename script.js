050dfbad00b6fd5362c29c7dd8b0c34330623c15
eotuzz <arthurfmmband@gmail.com>
2024-03-20 09:09:43 -0300

    aaaa

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-console.log("teste")
+//let a = "13";
+//let b = 2;
+//let resultado = a + b;
+//let resultado1 = a - b;
 
-var teste;
+
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+let resultadoTexto = texto1 + " , " + texto2 + "akjsdklasjdlk" + " lkjsalkdjalksd"
+console.log(resultadoTexto);
+
+
+
+
+
+/*
+console.log(resultado);
+console.log(resultado1)
+console.log(typeof(resultado))
+console.log(typeof(resultado1))
+console.log(typeof(a));
+console.log(typeof(b));
+*/
